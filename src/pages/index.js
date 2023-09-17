02c74de9f4dca8c100bc9764d59122586c080ed5
canhth <trancanh1201@gmail.com>
2023-09-17 13:33:31 +0700

    create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,33 @@
 import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import Link from '@docusaurus/Link';
 import styles from './index.module.css';
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function HomepageBackgroundWithTitle() {
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
+    <section className={styles.background}>
+      <img className={styles.backgroundImg} src='img/img-background.jpg'></img>
+      <div className={styles.titleOnImg}>
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
+        <Link
+          className="button button--secondary button--lg"
+          to="/docs/intro">
+          Get started
+        </Link>
       </div>
-    </header>
+    </section>
   );
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title="Hello"
+      description="">
+      <HomepageBackgroundWithTitle />
     </Layout>
   );
 }
